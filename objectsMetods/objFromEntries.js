605ecc0315554fbffa260723665e3cabe745a336
Kozyro Vladimir <kozyrovladimir@gmail.com>
2021-02-27 01:23:09 +0300

    traning metod of object fromEntries

---
+++ objectsMetods/objFromEntries.js
@@ -0,0 +1,9 @@
+const myArr = [['name', 'Vladimir'],['age', 25],['isMarried', true]];
+
+const newObj = Object.fromEntries(myArr);
+
+console.log(newObj);
+
+/*
+Не знаю почему, но код выше не работает.
+*/
