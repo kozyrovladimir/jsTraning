35fd86aa26ca77565e4b58c1c111f84272bd14c7
Kozyro Vladimir <kozyrovladimir@gmail.com>
2021-02-26 15:28:44 +0300

    array metod forEach traning

---
+++ arraysMetods/forEach.js
@@ -0,0 +1,7 @@
+const myArr = [5, 6, 58, 41, 12, 94]; // Массив чисел
+
+let newArr = []; // Пустой массив
+
+myArr.forEach((item) => newArr.push(item * 2)); // Метод массива forEach, который добавляет в пустой макссив newArr каждый элемент массива newArr увеличенный вдвое
+
+console.log(newArr); // Выводим измененный массив newArr в консоль
